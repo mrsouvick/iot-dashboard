75b2a8d27d6ec913b8de3ac7d5677d6e7900b86a
Souvick Kumar Halder <reach.souvick@gmail.com>
2025-07-13 00:46:46 +0530

    code change

---
+++ script.js
@@ -0,0 +1,23 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAN1uveGFgEm8z15OjvqeNy9achvhZvEJ8",
+  authDomain: "smart-climate-monitor.firebaseapp.com",
+  databaseURL: "https://smart-climate-monitor-default-rtdb.firebaseio.com/",
+  projectId: "smart-climate-monitor"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+// Real-time Listener
+const tempElement = document.getElementById("temp");
+const humElement = document.getElementById("hum");
+
+db.ref("sensor/temperature").on("value", (snapshot) => {
+  tempElement.innerText = snapshot.val().toFixed(2);
+});
+
+db.ref("sensor/humidity").on("value", (snapshot) => {
+  humElement.innerText = snapshot.val().toFixed(2);
+});
